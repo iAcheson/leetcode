700cfa924269a5a3e2413d3538995c35a88ab8f7
Yunsheng Liu <yunsheng_liu@qq.com>
2021-01-05 22:03:11 +0800

    1.两数之和

--- 1. 两数之和.py
+++ 1. 两数之和.py
@@ -17,23 +17,57 @@
 输出：[0,1]
 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
 
-来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/two-sum
-著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 """
+
+
+# 方法一：暴力枚举
+#
+# 最容易想到的方法是枚举数组中的每一个数 x，寻找数组中是否存在 target - x。
+# 当我们使用遍历整个数组的方式寻找 target - x 时，需要注意到每一个位于 x
+# 之前的元素都已经和 x 匹配过，因此不需要再进行匹配。而每一个元素不能被使用
+# 两次，所以我们只需要在 x 后面的元素中寻找 target - x。
+
+# 复杂度分析
+#
+# 时间复杂度：O(N^2)，其中 N 是数组中的元素数量。最坏情况下数组中任意两个数
+# 都要被匹配一次。
+#
+# 空间复杂度：O(1)。
+
+
+class Solution1:
+    def twoSum(self, nums, target):
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
+
+
+# 方法二：哈希表
+#
+# 注意到方法一的时间复杂度较高的原因是寻找 target - x 的时间复杂度过高。因此，
+# 我们需要一种更优秀的方法，能够快速寻找数组中是否存在目标元素。如果存在，我们
+# 需要找出它的索引。
+#
+# 使用哈希表，可以将寻找 target - x 的时间复杂度降低到从 O(N) 降低到 O(1)O(1)。
+# 这样我们创建一个哈希表，对于每一个 x，我们首先查询哈希表中是否存在 target - x，
+# 然后将 x 插入到哈希表中，即可保证不会让 x 和自己匹配。
+
+# 复杂度分析
+# 
+# 时间复杂度：O(N)，其中 N 是数组中的元素数量。对于每一个元素 x，我们可以 O(1)地
+# 寻找 target - x。
+#
+# 空间复杂度：O(N)，其中 N 是数组中的元素数量。主要为哈希表的开销。
 
 
 class Solution:
     def twoSum(self, nums, target):
-        """
-
-        :param nums: list, 整数数组。
-        :param target: int, 整数目标值
-        :return:list
-        """
-        temp = {}
-        for i, value in enumerate(nums):
-            if target - value in temp:
-                return [temp[target - value], i]
-            temp[value] = i
+        hashtable = dict()
+        for i, num in enumerate(nums):
+            if target - num in hashtable:
+                return [hashtable[target - num], i]
+            hashtable[nums[i]] = i
         return []
